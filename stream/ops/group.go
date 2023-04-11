a61c38a5ddf367988855ea4ae7d1ba0c9c270266
yusaint <yusaint@github.com>
2023-04-11 11:03:59 +0800

    group 支持返回错误

--- stream/ops/group.go
+++ stream/ops/group.go
@@ -1,10 +1,13 @@
 package ops
 
-type KeyFunc[T any, R comparable] func(T) R
+import "sync"
+
+type KeyFunc[T any, R comparable] func(T) (R, error)
 
 type _group[T any, R comparable] struct {
 	AbstractOp
 	fn     KeyFunc[T, R]
+	locker sync.RWMutex
 	values map[R][]T
 }
 
@@ -19,7 +22,12 @@ func (f *_group[T, R]) End() (any, error) {
 	return f.downstream.End()
 }
 func (f *_group[T, R]) Accept(a any) (err error) {
-	key := f.fn(a.(T))
+	key, err := f.fn(a.(T))
+	if err != nil {
+		return err
+	}
+	f.locker.Lock()
+	defer f.locker.Unlock()
 	if _, isOK := f.values[key]; !isOK {
 		f.values[key] = make([]T, 0, 10)
 	}
